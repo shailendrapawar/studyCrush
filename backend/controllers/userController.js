e44010c3bdd82ef0c2a56849be0de1ec7025bae1
shailendr pawar <shailendrapawar980@gmail.com>
2025-04-12 00:34:35 +0530

    Fixed : error handling in login api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ configDotenv();
 
 class UserController {
 
-    
+
     static register = async (req, res) => {
 
         try {
@@ -16,7 +16,7 @@ class UserController {
 
             if (!name || !username || !email || !password) {
                 return res.status(400).json({
-                    msg: "feild missing value",
+                    msg: "Input fields are missing.",
                     success: false
                 })
             }
@@ -60,43 +60,62 @@ class UserController {
 
     static login = async (req, res) => {
 
-        const {identifier,password}=req.body;
+        try {
 
-        const isExist = await UserModel.findOne({ $or: [{ email:identifier }, { username:identifier }] });
+            const { identifier, password } = req.body;
 
-        if(!isExist){
-            return res.status(400).json({
-                msg: "User not found ",
+            if (!identifier || !password) {
+                return res.status(400).json({
+                    msg: "Input fields are missing.",
+                    success: false
+                })
+            }
+
+            const isExist = await UserModel.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+
+            if (!isExist) {
+                return res.status(400).json({
+                    msg: "User not found ",
+                    success: false
+                })
+            }
+
+            //  compare password ==============
+            const isMatch = await bcrypt.compareSync(password, isExist.password);
+            if (!isMatch) {
+                return res.status(401).json({
+                    msg: "invalid credentials ",
+                    success: false
+                })
+            }
+
+            // creating JWT token============
+            const token = await jwt.sign({ userId: isExist._id }, process.env.JWT_SECRET_TOKEN, {
+                expiresIn: 7 * 24 * 60 * 60 * 1000
+            })
+
+            const userData = isExist.toObject();
+            delete userData.password;
+
+            res.cookie("token", token, {
+                httpOnly: true,
+                sameSite: "none",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            }).json({
+
+                msg: "Login successfull",
+                user: userData
+            })
+
+        } catch (err) {
+
+            console.log("login error", err);
+            return res.status(500).json({
+                msg: "Something went wrong",
                 success: false
             })
+
         }
-
-        //  compare password ==============
-        const isMatch=await bcrypt.compareSync(password,isExist.password);
-        if(!isMatch){
-            return res.status(401).json({
-                msg: "invalid credentials ",
-                success: false
-            })
-        }
-
-        // creating JWT token============
-         const token =await jwt.sign({userId:isExist._id},process.env.JWT_SECRET_TOKEN,{
-            expiresIn:7*24*60*60*1000
-         })
-
-         const userData=isExist.toObject();
-         delete userData.password;
-        
-         res.cookie("token",token,{
-            httpOnly:true,
-            sameSite:"none",
-            maxAge:7*24*60*60*1000
-         }).json({
-
-            msg:"Login successfull",
-            user:userData
-         })
 
     }
 
