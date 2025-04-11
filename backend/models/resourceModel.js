1ba6ac8dd281ba876435cf6c22db9f09c12af690
shailendr pawar <shailendrapawar980@gmail.com>
2025-04-11 21:32:29 +0530

    Added: resource model in backend

---
+++ backend/models/resourceModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose"
+
+const commentSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    comment:{
+        type:String,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+
+
+const resourceSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        trim: true
+    },
+
+    link: {
+        type: String,
+        required: true
+    },
+
+    thumbnail: {
+        type: String,
+        default: "" // optional thumbnail if applicable
+    },
+
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    tags: [{
+        type: String,
+        trim: true
+    }],
+
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+
+    comments:[commentSchema],
+
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+export default mongoose.model("Resource",resourceSchema);
