ea10a1831481e0f0889ef3a6173ebc87653adc1e
shailendr pawar <shailendrapawar980@gmail.com>
2025-04-12 12:23:45 +0530

    ADDED: api for deleting resource

--- backend/controllers/resourceController.js
+++ backend/controllers/resourceController.js
@@ -1,22 +1,20 @@
+import resourceModel from "../models/resourceModel.js";
 import ResourceModel from "../models/resourceModel.js";
 class ResourceController {
 
     static createResource = async (req, res) => {
 
         try {
-
-            console.log(req.id)
+            // console.log(req.id)
             const { title, description, link, tags } = req.body;
 
-            if(!title||  !description || !link || !tags){
-                
+            if (!title || !description || !link || !tags) {
+
                 return res.status(400).json({
                     msg: "Input feilds are missing",
                     success: false
                 })
             }
-
-
             // console.log(req.body);
             const newResource = new ResourceModel({
                 title,
@@ -28,7 +26,7 @@ class ResourceController {
 
             const isCreated = await newResource.save();
 
-            if(!isCreated){
+            if (!isCreated) {
                 return res.status(400).json({
                     msg: "resource not created",
                     success: false
@@ -40,7 +38,7 @@ class ResourceController {
                 success: true
             })
 
-        } catch (err){
+        } catch (err) {
             return res.status(400).json({
                 msg: "something went wrong",
                 success: false
@@ -49,6 +47,46 @@ class ResourceController {
         }
 
     }
+
+    static deleteResource = async (req, res) => {
+
+        try {
+
+            const userId = req.id;
+            const { resourceId } = req.params
+
+            const isExist = await ResourceModel.findById(resourceId);
+
+            if (!isExist) {
+                return res.status(400).json({
+                    msg: "resource not found",
+                    success: false
+                })
+            }
+
+            //   check if user is owner of reosurce========
+            if (isExist.uploadedBy.toString() ==! userId) throw new Error(" not allowed");
+
+            const isDeleted=await resourceModel.findByIdAndDelete(resourceId);
+
+            return res.status(200).json({ message: "Resource deleted successfully" });
+
+        } catch (err) {
+
+            return res.status(400).json({
+                msg: "something went wrong",
+                success: false,
+                err
+            })
+
+        }
+
+
+
+
+
+    }
+
 
 }
 
--- backend/routes/resourceRoutes.js
+++ backend/routes/resourceRoutes.js
@@ -6,6 +6,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 const resourceRouter=express.Router();
 
 resourceRouter.post("/createResource",authMiddleware,ResourceController.createResource);
+resourceRouter.post("/deleteResource/:resourceId",authMiddleware,ResourceController.deleteResource);
 
 
 
